989f7b6c80cfdaaf5f1985794a21adbec5830f0c
letyresina <leticiaresina@gmail.com>
2021-12-01 16:44:09 -0300

    Criando as models Editora e generoe a pasta 'Images'

---
+++ SpringLibrary/Models/Editora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpringLibrary.Models
+{
+    public class Editora
+    {
+        [Display(Name = "Código da editora")]
+        public int editCod { get; set; }
+
+        [Display(Name = "Nome da editora")]
+        public string editNome { get; set; }
+    }
+}
---
+++ SpringLibrary/Models/Genero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpringLibrary.Models
+{
+    public class Genero
+    {
+        [Display(Name = "Código do gênero do livro")]
+        public int genCod { get; set; }
+
+        [Display(Name = "Nome do gênero do livro")]
+        public string genNome { get; set; }
+    }
+}
