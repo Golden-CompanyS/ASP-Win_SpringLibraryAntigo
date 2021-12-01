1fedb3121a5ae46ace508b09b0bdeba32d8070a3
letyresina <leticiaresina@gmail.com>
2021-12-01 15:38:02 -0300

    Model Produto criada

--- SpringLibrary/Models/Produto.cs
+++ SpringLibrary/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,16 @@ namespace SpringLibrary.Models
 {
     public class Produto
     {
+        [Display(Name = "Código do produto")]
+        public int prodCod { get; set; }
+
+        [Display(Name = "Nome do produto")]
+        public string prodNome { get; set; }
+
+        [Display(Name = "Preço do produto")]
+        public decimal prodPreco { get; set; }
+
+        [Display(Name = "Quantidade no estoque")]
+        public int prodQtdEst { get; set; }
     }
 }
