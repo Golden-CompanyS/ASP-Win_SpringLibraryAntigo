46d7e3cf581873eda564b6f9ba4751605a27b2c1
letyresina <leticiaresina@gmail.com>
2021-12-02 12:43:48 -0300

    Model Nota Fiscal criada

---
+++ SpringLibrary/Models/NotaFiscal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpringLibrary.Models
+{
+    public class NotaFiscal
+    {
+        [Display(Name = "Código da Nota Fiscal")]
+        public int notaCod { get; set; }
+
+        [Display(Name = "Data de emissão da Nota Fiscal")]
+        public DateTime notaDtEmiss { get; set; }
+
+        [Display(Name = "Código do Funcionário")]
+        public int funcCod { get; set; }
+    }
+}
