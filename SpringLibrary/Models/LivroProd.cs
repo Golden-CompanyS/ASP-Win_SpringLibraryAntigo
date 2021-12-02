a8d15e240cda22371241b679cb01711a615ee056
letyresina <leticiaresina@gmail.com>
2021-12-02 12:37:11 -0300

    Models LivroProd e SouvProd criadas

---
+++ SpringLibrary/Models/LivroProd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpringLibrary.Models
+{
+    public class LivroProd
+    {
+        [Display(Name = "ISBN do livro")]
+        public string isbn { get; set; }
+
+        [Display(Name = "Código do produto")]
+        public int prodCod { get; set; }
+    }
+}
---
+++ SpringLibrary/Models/SouvProd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpringLibrary.Models
+{
+    public class SouvProd
+    {
+        [Display(Name = "Código do Souvenir")]
+        public int souvCod { get; set; }
+
+        [Display(Name = "Código do produto")]
+        public int prodCod { get; set; }
+    }
+}
