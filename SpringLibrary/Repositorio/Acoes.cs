0c39f5b5ca6079621b32802bc77268a811416e40
letyresina <leticiaresina@gmail.com>
2021-12-03 18:53:09 -0300

    Criação da pasta Repositorio, classes Conexao e Acoes. Programado já a conexão com o banco

---
+++ SpringLibrary/Repositorio/Acoes.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringLibrary.Repositorio
+{
+    public class Acoes
+    {
+        MySqlConnection cn = new MySqlConnection("Server=localhost;Database=dbspringlibrary;user=root;pwd=leleca0404");
+        public static string msg;
+        public MySqlConnection ConectarBD()
+        {
+            try
+            {
+                cn.Open();
+            }
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao conectar" + erro.Message;
+            }
+            return cn;
+        }
+
+        public MySqlConnection DesconectarBD()
+        {
+            try
+            {
+                cn.Close();
+            }
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao desconectar" + erro.Message;
+            }
+            return cn;
+        }
+    }
+}
