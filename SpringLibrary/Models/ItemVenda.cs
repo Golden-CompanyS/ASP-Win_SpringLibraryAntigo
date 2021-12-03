af5cf3edd4c287bfcb5c20414dcb13340b3ce4c8
letyresina <leticiaresina@gmail.com>
2021-12-03 17:25:02 -0300

    Model ItemVenda criada

--- SpringLibrary/Models/ItemVenda.cs
+++ SpringLibrary/Models/ItemVenda.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,22 @@ namespace SpringLibrary.Models
 {
     public class ItemVenda
     {
+        [Display(Name = "Código de Item Venda")]
+        public int itVendaCod { get; set; }
+
+        [Display(Name = "Código do produto")]
+        public int prodCod { get; set; }
+
+        [Display(Name = "Quantidade de itens da venda")]
+        public int itVendaQtd { get; set; }
+
+        [Display(Name = "Código da Nota Fiscal")]
+        public int notaCod { get; set; }
+
+        [Display(Name = "Código da Locação")]
+        public int locDesc { get; set; }
+
+        [Display(Name = "Código da Restauração")]
+        public int restCod { get; set; }
     }
 }
